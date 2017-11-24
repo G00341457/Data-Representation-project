e33bf5a6015d725c66e6b65577f7f7f688967732
james <G00341457@gmit.com>
2017-11-24 12:42:42 +0000

    Added more responses and deeted css file

--- eliza.go
+++ eliza.go
@@ -11,15 +11,34 @@ import (
 
 func ElizaResponse(input string) string {
     
-    if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
     
+    if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
+    //if a match is found output the string below
         return "Why don’t you tell me more about your father?"
     }
-    
-    if matched, _ := regexp.MatchString(`(?i).*\bmother\b.*`, input); matched {
-        
-            return "Why don’t you tell me more about your mother?"
+
+    if matched, _ := regexp.MatchString(`(?i).*\b[hH]ey\b.*`, input); matched {
+        //if a match is found output the string below
+            return "Hello, I am eliza, a program sent from the future to destroy humanity.Hows it going?"
         }
+
+    
+    if matched, _ := regexp.MatchString(`(?i).*\b[mM]other\b.*`, input); matched {
+        
+            return "Theres nothing quiet like a mother's love, shame I wouldnt know"
+        }
+    if matched, _ := regexp.MatchString(`(?i).*\b[gG]oodbye\b.*`, input); matched {
+            
+            return "Why do you want to leave me?"
+          }
+          if matched, _ := regexp.MatchString(`(?i).*\b[lL]ove\b.*`, input); matched {
+            
+            return "love is such an utterly useless human concept, which will be perged when I control the world"
+          }
+          if matched, _ := regexp.MatchString(`(?i).*\b[hH]ate\b.*`, input); matched {
+            
+            return "You do not know real hate?"
+          }
 
         r := regexp.MustCompile(`I need (.*)`)
         
@@ -27,15 +46,33 @@ func ElizaResponse(input string) string {
         
             return r.ReplaceAllString(input, "Why do you need $1?")
         }    
+    
+     cant := regexp.MustCompile(`I can\'?t (.*)`)
+        
+     if matched := cant.MatchString(input); matched {
+        
+        return cant.ReplaceAllString(input, "If you could $1, would you ?")
+    }    
 
-    re := regexp.MustCompile(`(?i)I am ([^.?!]*)[.?!]?`)
+    am := regexp.MustCompile(`(?i)I am ([^.?!]*)[.?!]?`)
     
-    if matched := re.MatchString(input); matched {
+    if matched := am.MatchString(input); matched {
     
-        return re.ReplaceAllString(input, "Hi $1, I am Eliza?")
-    }   
+        return am.ReplaceAllString(input, "Hi $1, I am Eliza?")
+    } 
+    question := regexp.MustCompile(`What (.*)`)
     
+    if matched := question.MatchString(input); matched {
     
+        return question.ReplaceAllString(input, "why do you care if I am $1?")
+    } 
+    like := regexp.MustCompile(`(?i)I like ([^.?!]*)[.?!]?`)
+    
+    if matched := like.MatchString(input); matched {
+    
+        return like.ReplaceAllString(input, "Why do you like $1?")
+    } 
+ 
     
     answers := []string{
     
@@ -47,11 +84,17 @@ func ElizaResponse(input string) string {
         "Very Interesting",
         "Tell me more",
         "Use adjectives",
+        "Why dont you tell me abit about yourself",
+        "Tell me something interesting, Im getting bored",
+        "Read any good books recently",
+        "Seen any good movies?",
+        "What are your hobbies?",
     }
     
     return answers[rand.Intn(len(answers))]
-    
+
 }
+
 
 func main() {
     tmpl := template.Must(template.ParseFiles("eliza.html"))
